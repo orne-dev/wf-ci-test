0ae5a9d7eefa0623a64ce6ac3299178f27b4064a
(w) Iker Hernaez <wamphiry@orne.dev>
2021-03-21 21:58:55 +0100

    Created Maven project

---
+++ src/test/java/dev/orne/test/ghwf/SampleTest.java
@@ -0,0 +1,42 @@
+package dev.orne.test.ghwf;
+
+/*-
+ * #%L
+ * github-wf-test
+ * %%
+ * Copyright (C) 2019 - 2021 Orne Developments
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("ut")
+class SampleTest {
+
+    @Test
+    void testNoParam() {
+        assertEquals("Hello world!!", Sample.helloWorld(null));
+    }
+
+    @Test
+    void testName() {
+        assertEquals("Hello workflow!!", Sample.helloWorld("workflow"));
+    }
+}
